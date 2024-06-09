2246be1762d647ec0f8d4c2c7b26bf2bf3fb3c43
Katherina-B <79216681+Katherina-B@users.noreply.github.com>
2024-06-09 22:27:12 +0200

    Update interp.py

--- interp.py
+++ interp.py
@@ -1,19 +1,18 @@
+import matplotlib
+matplotlib.use('agg')
+
 import os
 import torch
 from captum.attr import IntegratedGradients
 import matplotlib.pyplot as plt
 import wandb
 import logging
-
 import json
 from typing import Dict, Tuple
 import random
 import time
-
 from captum.attr import IntegratedGradients
 import matplotlib.pyplot as plt
-
-
 import torch.nn as nn
 import torch.optim as optim
 import torchvision.transforms as transforms
@@ -30,27 +29,25 @@ with open("params.yaml", "r") as f:
     config = yaml.safe_load(f)
 
 ind = config["training"]["optimizer"]["lr"]
-
 wandb.init(
     # Set the project where this run will be logged
     project="lb5_intr",
-    # We pass a run name (otherwise it’ll be randomly assigned, like sunshine-lollypop-10)
+    # We pass a run name (otherwise it'll be randomly assigned, like sunshine-lollypop-10)
     name=f"run_{ind}",
     # Track hyperparameters and run metadata
     config={
-    "learning_rate": config["training"]["optimizer"],
-    "dataset": "Flower-102",
-    "epochs": config["training"]["epochs"],
+        "learning_rate": config["training"]["optimizer"],
+        "dataset": "Flower-102",
+        "epochs": config["training"]["epochs"],
     })
 
 def interpret_model(config):
     logger = logging.getLogger(__name__)
-    _, _, test_dataset = load_and_split_data(config["data"]["local_dir"])
+    *, *, test_dataset = load_and_split_data(config["data"]["local_dir"])
     test_loader = DataLoader(test_dataset, batch_size=1)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model, optimizer, loss_fn = create_model()
     model = model.to(device)
-    
     ig = IntegratedGradients(model)
     output_dir = "interpretation_results"
     os.makedirs(output_dir, exist_ok=True)
@@ -58,7 +55,6 @@ def interpret_model(config):
         images, labels = images.to(device), labels.to(device)
         attributions, delta = ig.attribute(images, target=labels, return_convergence_delta=True)
         for i in range(len(images)):
-            images, labels = images.cpu(), labels.cpu()
             attr_img = attributions[i].cpu().detach().numpy().transpose(1, 2, 0)
             plt.imshow(attr_img)
             plt.axis('off')
@@ -71,5 +67,3 @@ def interpret_model(config):
 if __name__ == "__main__":
     interpret_model(config)
     wandb.finish()
-    
-  
