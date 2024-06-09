eac90a77df3c94083a4edc3ede28ab8f9614faf6
Katherina-B <79216681+Katherina-B@users.noreply.github.com>
2024-06-09 22:51:43 +0200

    Update interp.py

--- interp.py
+++ interp.py
@@ -57,7 +57,7 @@ def interpret_model(config):
         _, preds = torch.max(outputs, 1)
         if preds != labels:
             incorrect_count += 1
-            if incorrect_count > 10:
+            if incorrect_count > 100:
                 break
             attributions, delta = ig.attribute(images, target=labels, return_convergence_delta=True)
             for i in range(len(images)):
