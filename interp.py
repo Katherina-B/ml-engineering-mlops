e3b84e6e54d67196c8f26db341684ba1b4fa3991
Katherina-B <79216681+Katherina-B@users.noreply.github.com>
2024-06-10 12:57:14 +0200

    Update interp.py

--- interp.py
+++ interp.py
@@ -2,7 +2,7 @@ import matplotlib
 matplotlib.use('agg')
 import os
 import torch
-from captum.attr import IntegratedGradients
+from captum.attr import GradientShap
 import matplotlib.pyplot as plt
 import wandb
 import logging
@@ -10,8 +10,6 @@ import json
 from typing import Dict, Tuple
 import random
 import time
-from captum.attr import IntegratedGradients
-import matplotlib.pyplot as plt
 import torch.nn as nn
 import torch.optim as optim
 import torchvision.transforms as transforms
@@ -20,7 +18,7 @@ import yaml
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 import torchvision.models as models
-from load_date import load_and_split_data
+from load_data import load_and_split_data
 from train import create_model
 import numpy as np
 
@@ -29,18 +27,13 @@ with open("params.yaml", "r") as f:
 
 ind = config["training"]["optimizer"]["lr"]
 wandb.init(
-    # Set the project where this run will be logged
     project="lb5_intr",
-    # We pass a run name (otherwise it'll be randomly assigned, like sunshine-lollypop-10)
     name=f"run_{ind}",
-    # Track hyperparameters and run metadata
     config={
         "learning_rate": config["training"]["optimizer"],
         "dataset": "Flower-102",
         "epochs": config["training"]["epochs"],
     })
-
-from captum.attr import Deconvolution
 
 def interpret_model(config):
     logger = logging.getLogger(__name__)
@@ -50,20 +43,22 @@ def interpret_model(config):
     model, optimizer, loss_fn = create_model()
     model = model.to(device)
 
-    deconv = Deconvolution(model)  # Initialize Deconvolution
-
     output_dir = "interpretation_results"
     os.makedirs(output_dir, exist_ok=True)
     incorrect_count = 0
     for images, labels in test_loader:
         images, labels = images.to(device), labels.to(device)
+        images.requires_grad_()
         outputs = model(images)
         _, preds = torch.max(outputs, 1)
         if preds != labels:
             incorrect_count += 1
             if incorrect_count > 100:
                 break
-            attributions = deconv.attribute(images, target=labels)  # Use Deconvolution for attribution
+            model.zero_grad()
+            loss = loss_fn(outputs, labels)
+            loss.backward()
+            attributions = images.grad  # Use Gradient * Input for attribution
 
             for i in range(len(images)):
                 attr_img = attributions[i].cpu().detach().numpy().transpose(1, 2, 0)
