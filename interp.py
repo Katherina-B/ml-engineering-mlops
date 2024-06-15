146277c4af906817b0195543124fa3beb9bed924
Katherina-B <79216681+Katherina-B@users.noreply.github.com>
2024-06-15 17:20:13 +0200

    Update interp.py

--- interp.py
+++ interp.py
@@ -2,7 +2,7 @@ import matplotlib
 matplotlib.use('agg')
 import os
 import torch
-from captum.attr import LayerGradCam, LayerAttribution
+from captum.attr import LayerGradCam, LayerAttribution, GuidedBackprop, IntegratedGradients
 from captum.attr import Occlusion
 import matplotlib.pyplot as plt
 import wandb
@@ -16,7 +16,6 @@ from torch.utils.data import DataLoader
 import torch
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
-from lime import lime_image
 
 with open("params.yaml", "r") as f:
     config = yaml.safe_load(f)
@@ -47,6 +46,8 @@ def interpret_model(config):
     model = model.to(device)
 
     grad_cam = LayerGradCam(model, model.layer4[-1])
+    guided_backprop = GuidedBackprop(model)
+    integrated_gradients = IntegratedGradients(model)
     occlusion = Occlusion(model)
 
     output_dir = "interpretation_results"
@@ -63,6 +64,8 @@ def interpret_model(config):
                 break
 
             grad_cam_attr = grad_cam.attribute(images, target=labels)
+            guided_backprop_attr = guided_backprop.attribute(images, target=labels)
+            integrated_gradients_attr = integrated_gradients.attribute(images, target=labels, n_steps=50)
 
             # Specify the sliding_window_shapes for Occlusion
             sliding_window_shapes = (3, images.shape[-2] // 8, images.shape[-1] // 8)
@@ -70,38 +73,28 @@ def interpret_model(config):
 
             for i in range(len(images)):
                 attr_img_grad_cam = grad_cam_attr[i].cpu().detach().numpy().transpose(1, 2, 0)
+                attr_img_guided_backprop = guided_backprop_attr[i].cpu().detach().numpy().transpose(1, 2, 0)
+                attr_img_integrated_gradients = integrated_gradients_attr[i].cpu().detach().numpy().transpose(1, 2, 0)
                 attr_img_occlusion = occlusion_attr[i].cpu().detach().numpy().transpose(1, 2, 0)
 
-                # Use LIME for interpretation
-                lime_explainer = lime_image.LimeImageExplainer()
-
-                def predict_fn(input_image):
-                    with torch.no_grad():
-                        input_tensor = torch.from_numpy(input_image).permute(2, 0, 1).unsqueeze(0).float().to(device)
-                        output = model(input_tensor)
-                        _, predicted = torch.max(output.data, 1)
-                        return predicted.item()
-
-                explanation = lime_explainer.explain_instance(images[i].cpu().detach().permute(1, 2, 0).numpy(), predict_fn, top_labels=5, hide_color=0, num_samples=1000)
-
-                lime_image = explanation.get_image_and_mask(explanation.top_labels[0], positive_only=True, num_features=5, hide_rest=True)
-
-                fig, ax = plt.subplots(1, 4, figsize=(24, 6))
+                fig, ax = plt.subplots(1, 5, figsize=(30, 6))
                 ax[0].imshow(images[i].cpu().detach().permute(1, 2, 0))
                 ax[0].axis('off')
                 ax[0].set_title('Original Image')
                 ax[1].imshow(attr_img_grad_cam, cmap='viridis')
                 ax[1].axis('off')
                 ax[1].set_title('Grad-CAM Attribution')
-                ax[2].imshow(attr_img_occlusion, cmap='viridis')
+                ax[2].imshow(attr_img_guided_backprop, cmap='viridis')
                 ax[2].axis('off')
-                ax[2].set_title('Occlusion Attribution')
-                ax[3].imshow(lime_image)
+                ax[2].set_title('Guided Backpropagation Attribution')
+                ax[3].imshow(attr_img_integrated_gradients, cmap='viridis')
                 ax[3].axis('off')
-                ax[3].set_title('LIME Explanation')
+                ax[3].set_title('Integrated Gradients Attribution')
+                ax[4].imshow(attr_img_occlusion, cmap='viridis')
+                ax[4].axis('off')
+                ax[4].set_title('Occlusion Attribution')
                 img_path = os.path.join(output_dir, f"combined_{incorrect_count}_{i}.png")
                 plt.savefig(img_path)
-                plt.close()
                 wandb.log({"combined_image": [wandb.Image(img_path, caption=f"Label: {labels[i].item()}")]})
                 os.remove(img_path)
 
